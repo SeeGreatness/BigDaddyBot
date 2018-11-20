96a431e549f9742a6bd87b03070a9498ddc20b44
chris laframboise <hire.code.seegreatness@gmail.com>
2018-11-20 16:56:59 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+   console.log(`${client.user.tag} Is Active!`);
+    client.user.setPresence({game: {name: `with you!`, type: 1}});
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+    client.user.setStatus("idle");
 });
 
 client.on('message', message => {
@@ -14,6 +17,14 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
+  	}
+client.on('message', message => {
+    if (message.content === 'cheyenne') {
+    	message.reply('Emma loves Cheyenne!');
+  	}
+client.on('message', message => {
+    if (message.content === 'Emma') {
+    	message.reply('Cheyenne loves emma!');
   	}
 });
 
